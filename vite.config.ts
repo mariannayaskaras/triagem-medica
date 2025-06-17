f4fade7f61ba24d2855717e2e37a183868354cf6
Marianna Yáskara <marianna.yaskara@live.com>
2025-06-17 12:10:24 -0300

    correção

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path' // ← necessário para usar path.resolve
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src') // ← aqui o alias é configurado
-    }
-  }
-})
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+  },
+});
